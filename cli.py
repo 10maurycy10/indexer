4f8cbe132af197f9c6a36c7421a984e718e2010c
10maurycy10 <10maurycy10@gmail.com>
2023-08-14 19:36:01 -0700

    work

--- cli.py
+++ cli.py
@@ -7,7 +7,6 @@ import tqdm
 
 db = data.DB()
 db.connect("db.sqlite")
-print("Opened database")
 
 # Setup argparse
 parser = argparse.ArgumentParser(prog="bulk tagger")
@@ -22,6 +21,7 @@ def subcommand(args=[], parent=subparsers):
         for arg in args:
             parser.add_argument(*arg[0], **arg[1])
     return decorator
+
 def argument(*name_or_flags, **kwargs):
     return ([*name_or_flags], kwargs)
 
@@ -38,18 +38,28 @@ def add_indexed(args):
     else:
         print(f"File/Directory {path} does not exit")
 
+@subcommand([argument("directory")])
+def rm_indexed(args):
+    path = os.path.abspath(args.directory)
+    db.cursor().execute("DELETE FROM indexed_dirs WHERE filepath = ?", (path,));
+    db.commit()
+
 def insert(metadata_dict):
-    for filepath in metadata_dict.keys():
+    print(f"Inserting {len(metadata_dict)}")
+    for filepath in list(metadata_dict.keys()):
         metadata = metadata_dict[filepath]
-        for key in metadata.keys():
+        for key in list(metadata.keys()):
             if metadata[key]:
                 value = metadata[key]
                 db.cursor().execute("INSERT INTO tags (filename, tagname, tagvalue) values (?,?,?);", (filepath, key, value))
                 db.cursor().execute("INSERT INTO searchindex (filename, tagvalue) values (?,?);", (filepath, value))
+            del metadata[key]
+        del metadata_dict[filepath]
 
 @subcommand([argument("--fulltext", action='store_true'), argument("--no-tempfile", action="store_true")])
 def freshen(args):
     cfg = meta.Cfg()
+    cfg.callback = insert
     if args.fulltext:
         cfg.fulltext = True
     if args.no_tempfile:
@@ -58,15 +68,20 @@ def freshen(args):
     to_index = db.get_indexed_dirs()
     files_to_index = []
     for dir_to_index in to_index[::-1]:
+        #files_to_index.append(dir_to_index)
         print(dir_to_index)
         for root, dirs, files in os.walk(dir_to_index, topdown=False):
-           for name in files:
+            print()
+            for name in files:
                 filepath = os.path.join(root, name)
                 if not db.indexed(filepath):
                     files_to_index.append(filepath)
+                else:
+                    print(f"Disregarding {filepath}")
 
     for filepath in tqdm.tqdm(files_to_index):
         metadata = {}
+        print(filepath)
         meta.dumpdata(filepath, metadata, cfg)
         insert(metadata)
         db.commit()
@@ -75,25 +90,53 @@ def freshen(args):
         print(cfg.error_counters[key])
 
 
-@subcommand([argument("keywords", nargs="*")])
+@subcommand([argument("keywords", nargs="*"), argument("-l","--limit", default=50)])
 #TODO implement intersection in sql
 def search(args):
+    """
+    Searches for keywords in index, by defualt shows the 50 most relevent results, this can be changed with --limit
+    """
     dbc = db.cursor()
     keywords = " ".join(args.keywords)
     print("Performing filename search...")
-    dbc.execute("SELECT filename FROM searchindex WHERE filename MATCH ? GROUP BY filename ORDER BY RANK;", (keywords,))
+    dbc.execute("SELECT filename FROM searchindex WHERE filename MATCH ? GROUP BY filename ORDER BY RANK LIMIT ?;", (keywords,args.limit))
     for (file,) in dbc:
         print("> ",file)
     print("Performing fulltext search...")
-    dbc.execute("SELECT filename FROM searchindex WHERE tagvalue MATCH ? GROUP BY filename ORDER BY RANK;", (keywords,))
+    dbc.execute("SELECT filename FROM searchindex WHERE tagvalue MATCH ? GROUP BY filename ORDER BY RANK LIMIT ?;", (keywords,args.limit))
     for (file,) in dbc:
         print("> ",file)
 
-@subcommand([])
+@subcommand([argument("--filename",required=False)])
 def drop(args):
-    db.cursor().execute("delete from tags")
-    db.cursor().execute("delete from searchindex")
+    """
+    Remove selected files from db
+    """
+    if args.filename:
+        db.cursor().execute("delete from tags where filename like ?", (f"{args.filename}%",))
+        db.cursor().execute("delete from searchindex where filename like ?", (f"{args.filename}%",))
+    else:
+        db.cursor().execute("delete from tags")
+        db.cursor().execute("delete from searchindex")
     db.commit()
+
+@subcommand([argument("filename")])
+def gettext(args):
+    """
+    Remove selected files from db
+    """
+    dbc = db.cursor()
+    dbc.execute("SELECT tagvalue FROM tags WHERE filename=? and tagname='fulltext';", (args.filename,))
+    for (x,) in dbc:
+        import sys
+        if type(x) == bytes:
+            try:
+                sys.stdout.write(x.decode("utf-8"))
+            except UnicodeDecodeError:
+                print("Falling back to latin-1")
+                sys.stdout.write(x.decode("latin-1"))
+        else:
+            print(x)
 
 if __name__ == "__main__":
     args = parser.parse_args()
